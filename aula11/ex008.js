92be4a7362db38bbe62fe01fed9d0ebe5999e58d
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-05-31 14:38:31 -0300

    Condições (Parte 1)

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 12
+console.log(`A velocidade do seu carro é ${velocidade}km`) 
+if(velocidade > 60) { // CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`) 
+    }
+console.log (`Dirija sempre usando o cinto de segurança!`)
